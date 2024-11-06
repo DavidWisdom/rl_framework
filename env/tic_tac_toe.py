fc438f75530aa41ec1fe1a39c10617607b035b80
davidwisdom <davidwisdom@foxmail.com>
2024-11-06 10:53:37 +0800

    Update

--- env/tic_tac_toe.py
+++ env/tic_tac_toe.py
@@ -72,27 +72,23 @@ class TicTacToe(Env):
         player_mark = "X"
         enemy_mark = "O"
         empty_mark = " "
-
         # 打印棋盘顶部
         print("    0   1   2")
         print("  +---+---+---+")
-
         # 打印棋盘的每一行
         for row in range(3):
             print(f"{row} | ", end="")
             for col in range(3):
                 # 根据 one-hot 编码确定标记
                 mark = empty_mark
-                if self.state_dict[0]["observation"][row * 3 + col] == 1:
+                if self.player_obs[0][row * 3 + col] == 1:
                     mark = player_mark
-                if self.state_dict[0]["observation"][row * 3 + col + 9] == 1:
+                if self.player_obs[1][row * 3 + col] == 1:
                     mark = enemy_mark
-
                 # 打印当前位置的标记
                 print(f"{mark} | ", end="")
             print()
             print("  +---+---+---+")
-
         # 打印棋盘底部
         print("    0   1   2")
         print("-----------------")
